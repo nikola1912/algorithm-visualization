debf3cd3b34eb0f9be8ba746ee5dec2e36d42e88
Nikola Jovanovic <nikola.jov1912@gmail.com>
2020-03-01 17:36:51 +0100

    Block sorting if array already sorted

--- src/sortingController.js
+++ src/sortingController.js
@@ -5,6 +5,7 @@ import sortingAlgorithms from "./sortingAlgorithms.js";
 const sortingController = (() => {
 
     let sortingState = false; //true = ON | false = OFF
+    let isSorted = false;
     let speed = 100;
     let maxSpeed = 700;
     const array = testObjects.testArray.slice();
@@ -16,14 +17,28 @@ const sortingController = (() => {
     const getMaxSpeed = () => maxSpeed;
 
     const handleSort = async () => {
-        sortingState = true;
-        let sortedArray = await sortingAlgorithms.bubbleSort(array);
-        sortingState = false;
-        console.log(sortedArray);
+        if (!sortingState && !isSorted) {
+            sortingState = true;
+            document.getElementById("unsortButton").disabled = true;
+            document.getElementById("playIcon").style.display = "none";
+            document.getElementById("pauseIcon").style.display = "block";
+    
+            let sortedArray = await sortingAlgorithms.bubbleSort(array);
+    
+            isSorted = true;
+            sortingState = false;
+            document.getElementById("unsortButton").disabled = false;
+            document.getElementById("playIcon").style.display = "inline";
+            document.getElementById("pauseIcon").style.display = "none";
+            console.log(sortedArray);
+        }
     };
 
     const handleUnsort = () => {
-        if (!sortingState) displayController.drawArray(array);
+        if (!sortingState) {
+            displayController.drawArray(array);
+            isSorted = false;   
+        }
     };
 
     const handleSpeedChange = () => {
